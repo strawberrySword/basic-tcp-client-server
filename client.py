72aa75c59191286030155f24d6c11fbcba97d723
D4niel0s <das.volkov@gmail.com>
2024-11-12 12:04:15 +0200

    Started working on client

---
+++ client.py
@@ -0,0 +1,20 @@
+import socket, sys
+
+
+def main():
+    host, port = ("127.0.0.1", 1337)
+    if len(sys.argv) > 1: #Host given
+        host = sys.argv[1] 
+    if len(sys.argv == 3): #Host and port given
+        port = sys.argv[2]
+
+    conn = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+
+    conn.connect((host,port))
+
+        
+
+
+
+if __name__ == '__main__':
+    main()
