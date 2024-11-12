7fd9f42b220b6e597ac473649b51488f16954a33
D4niel0s <das.volkov@gmail.com>
2024-11-12 17:20:04 +0200

    Added command execution

--- utils.py
+++ utils.py
@@ -29,19 +29,38 @@ def send_all(sock, message):
 Gets a string that represents a command call (ex: "calculate: 2*5")
 '''
 def execute_command(command):
-    pass
-
     params = command.split(':')
 
     if params[0] == "calculate":
-        pass
+        return evaluate_exp(params[1].lstrip().rstrip().split(' '))
     elif params[0] == "max":
-        pass
+        return f'the maximum is {max(params[1].lstrip().rstrip().split(' '))}'
     elif params[0] == "factors":
-        pass
+        return f'the prime factors of {params[1]} are: {[i for i in range(1, int(params[1])+1) if int(params[1]) % i == 0].join(' ')}'
     else:
         return
 
+
+'''
+Evaluates an expression of the form X Y Z, where Y is an operator, X Z are signed ints
+'''
+def evaluate_exp(expression):
+    parts = expression.split(' ')
+    x = int(parts[0])
+    z = int(parts[2])
+
+    if parts[1] == '+':
+        res = str(x+z)
+    elif parts[1] == '-':
+        res = str(x-z)
+    elif parts[1] == '*':
+        res = str(x*z)
+    elif parts[1] == '/':
+        res = str(x/z)
+    elif parts[1] == '^':
+        res = str(x**z)
+
+    return f'response: {res}.'
 
 
 class Client: 
