782eea369215bfac646f888e8ba8011c0f4ba878
D4niel0s <das.volkov@gmail.com>
2024-11-16 02:27:50 +0200

    Client works, code NOT clean atm

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import socket, sys
+import struct
 from utils import *
 
 def main():
@@ -14,23 +15,31 @@ def main():
 
     conn.connect((host,port))
 
-    print(conn.recv(10000)) #Get welcome message
+    print(recvall(conn, struct.unpack('>I', recvall(conn,4))[0])) #Get welcome message
     user = input()[5:].strip()
     password = input()[9:].strip()
     send_all(conn ,user+' '+password)
 
+    m = 'Failed to login.'
+    resp = recvall(conn, struct.unpack('>I', recvall(conn,4))[0]).decode('utf-8')
+    print(resp)
 
-    if conn.recv(10000) == 'Failed to login.':
-        print("Please retry logging in:")
+    while resp == m:
+        print("Please try logging in again:")
         user = input()[5:].strip()
         password = input()[9:].strip()
         send_all(conn, user+' '+password)
 
+        resp = recvall(conn, struct.unpack('>I', recvall(conn,4))[0]).decode('utf-8')
+        print(resp)
+
     usr_in = input()
     while(usr_in != 'quit'):
         send_all(conn, usr_in)
+        print(recvall(conn, struct.unpack('>I', recvall(conn,4))[0]).decode('utf-8'))
         usr_in = input()
 
+    conn.close()
 
 if __name__ == '__main__':
     main()
