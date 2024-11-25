a7c99d53195817a2ae4773d2416d7b6c46cdc80e
D4niel0s <das.volkov@gmail.com>
2024-11-25 22:08:32 +0200

    Changed filename and added comments

--- numbers_client.py
+++ numbers_client.py
@@ -5,26 +5,29 @@ import struct
 from utils import *
 
 def main():
-    host, port = ("127.0.0.1", 1337)
+    HOST, PORT = ("127.0.0.1", 1337)
     if len(sys.argv) > 1: #Host given
-        host = sys.argv[1] 
+        HOST = sys.argv[1] 
     if len(sys.argv) == 3: #Host and port given
-        port = int(sys.argv[2])
+        PORT = int(sys.argv[2])
 
     conn = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
 
-    conn.connect((host,port))
+    conn.connect((HOST,PORT))
 
-    print(recvall(conn, struct.unpack('>I', recvall(conn,4))[0])) #Get welcome message
+    print(recvall(conn, struct.unpack('>I', recvall(conn,4))[0]).decode('utf-8')) #Get welcome message
+
+    #Insert login credentials: login is assumed to be in the specified format. Otherwise, the sent credentials will be truncated
     user = input()[5:].strip()
     password = input()[9:].strip()
     send_all(conn ,user+' '+password)
 
-    m = 'Failed to login.'
+    #Recieve login success/failure
     resp = recvall(conn, struct.unpack('>I', recvall(conn,4))[0]).decode('utf-8')
     print(resp)
 
-    while resp == m:
+    #Rety login
+    while resp == 'Failed to login.':
         print("Please try logging in again:")
         user = input()[5:].strip()
         password = input()[9:].strip()
@@ -33,6 +36,7 @@ def main():
         resp = recvall(conn, struct.unpack('>I', recvall(conn,4))[0]).decode('utf-8')
         print(resp)
 
+    #Send commands and get server's response
     usr_in = input()
     while(usr_in != 'quit'):
         send_all(conn, usr_in)
